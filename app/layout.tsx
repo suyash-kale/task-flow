7b865328cd35efa13b5fbf92e2aecd408a9a77a3
Suyash Kale <skale@global.sial.com>
2024-03-27 12:54:24 +0530

    auth changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Provider>{children}\ </Provider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,25 @@
+'use client';
 import { ModeToggle } from '@/components/toggle-mode';
 import { Button } from '@/components/ui/button';
-import { auth, signIn, signOut } from '../auth';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
-export default async function Home() {
-  const session = await auth();
-  // console.log('Suyash', session);
+export default function Home() {
+  const { data } = useSession();
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-background">
       <h1 className="text-5xl font-bold text-primary">
-        Welcome {session?.user?.name} to Task Flow
+        Welcome {data?.user?.name} to Task Flow
       </h1>
 
       <p className="text-xl py-3 text-secondary">
         Your own Task Management System
       </p>
-      {session?.user ? (
-        <form
-          action={async () => {
-            'use server';
-            await signOut();
-          }}
-        >
+      {data?.user ? (
+        <form action={() => signOut()}>
           <Button type="submit">Sign Out</Button>
         </form>
       ) : (
-        <form
-          action={async () => {
-            'use server';
-            await signIn('google');
-          }}
-        >
+        <form action={() => signIn('google')}>
           <Button type="submit">Sign In</Button>
         </form>
       )}
